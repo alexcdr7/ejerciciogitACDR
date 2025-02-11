6f06e88872858e5cca1fd1de96ba8c52482b2203
alexcdr7 <2alesandrioo@gmail.com>
2025-02-11 14:20:37 +0100

    se repite el script e itero cada imagen

--- virus.bat
+++ virus.bat
@@ -4,3 +4,7 @@ setlocal enabledelayedexpansion
 set images=imagen1.jpg imagen2.jpg imagen3.jpg imagen4.jpg imagen5.jpg
 
 set count=0
+
+:loop
+for %%i in (%images%) do (
+    start "" "%%i"  REM Abre la imagen con el visor predeterminado
