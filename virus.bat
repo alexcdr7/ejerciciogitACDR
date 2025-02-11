ac864774bf8245fc02341f8745a42ef31daeef7f
alexcdr7 <2alesandrioo@gmail.com>
2025-02-11 14:29:08 +0100

    acabo y no dejo que haya más de 10 imagenes para que no pete la prueba

--- virus.bat
+++ virus.bat
@@ -9,6 +9,14 @@ set count=0
 for %%i in (%images%) do (
     start "" "%%i"  REM Abre la imagen con el visor predeterminado
     echo Esperando que cierres la imagen...
-    timeout /t 1 >nul
+    timeout /t 0,1 >nul
     taskkill /IM "Microsoft.Photos.exe" /F >nul 2>&1
     taskkill /IM "rundll32.exe" /F >nul 2>&1
+    set /a count+=1
+if !count! GEQ 15 goto end  REM Termina después de 10 imágenes
+)
+
+goto loop
+
+:end
+exit
